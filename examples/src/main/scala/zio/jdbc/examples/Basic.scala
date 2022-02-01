3e26abb8d84018cfd42ef6bdd4761f8569884f0e
John A. De Goes <john@degoes.net>
2022-02-01 22:30:53 +0000

    More examples

--- examples/src/main/scala/zio/jdbc/examples/Basic.scala
+++ examples/src/main/scala/zio/jdbc/examples/Basic.scala
@@ -1,11 +1,23 @@
 package zio.jdbc.examples
 
+import zio._
 import zio.jdbc._
 
 object Basic {
-  val ex1 = sql"select * from users"
+  val age = 42
 
+  // Creating SQL statements using interpolation:
+  val ex1 = sql"select * from users where age = $age"
+
+  // Selecting into tuples:
   val ex2 = sql"select name, age from users".as[(String, Int)]
 
+  // Inserting from tuples:
   val ex3 = sql"insert into users ('name', 'age')".values(("John", 42))
+
+  // Executing statements:
+  val res1: ZIO[ZConnectionPool, Throwable, Option[(String, Int)]] =
+    transaction {
+      selectOne(sql"select name, age from users where name = 'Sherlock Holmes'".as[(String, Int)])
+    }
 }
