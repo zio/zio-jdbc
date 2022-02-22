98e5924615d3756c0181482d61cf543bc6f84fb5
yisraelu <ysrlunion@gmail.com>
2022-02-21 19:22:51 -0500

    add test for extra Syntax object bug

--- core/src/test/scala/zio/jdbc/SqlSpec.scala
+++ core/src/test/scala/zio/jdbc/SqlSpec.scala
@@ -15,6 +15,11 @@ object SqlSpec extends ZIOSpecDefault {
             sql"select name, age from users where id = ${id}".toString ==
               s"Sql(select name, age from users where id = ?, $id)"
           )
+        } +
+        test("ensure no empty Syntax instances") {
+          val age  = 42
+          val name = "sholmes"
+          assertTrue(sql"select name, age from users where age = $age and name = $name".segments.size == 4)
         }
     }
 }
