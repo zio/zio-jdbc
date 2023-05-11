2e84846a818d94c578305885a92d5496b5db9ff2
Adam Fraser <adam.fraser@gmail.com>
2023-05-11 20:50:52 +0100

    cleanup

--- core/src/main/scala/zio/jdbc/ZConnection.scala
+++ core/src/main/scala/zio/jdbc/ZConnection.scala
@@ -113,7 +113,95 @@ object ZConnection {
     private[this] val initialSchema               = underlying.getSchema()
     private[this] val initialTransactionIsolation = underlying.getTransactionIsolation()
 
-    def restore(): Unit = {
+    def abort(executor: java.util.concurrent.Executor): Unit                                                          =
+      underlying.abort(executor)
+    def clearWarnings(): Unit                                                                                         =
+      underlying.clearWarnings()
+    def close(): Unit                                                                                                 =
+      underlying.close()
+    def commit(): Unit                                                                                                =
+      underlying.commit()
+    def createArrayOf(typeName: String, elements: Array[Object]): java.sql.Array                                      =
+      underlying.createArrayOf(typeName, elements)
+    def createBlob(): java.sql.Blob                                                                                   =
+      underlying.createBlob()
+    def createClob(): java.sql.Clob                                                                                   =
+      underlying.createClob()
+    def createNClob(): java.sql.NClob                                                                                 =
+      underlying.createNClob()
+    def createSQLXML(): java.sql.SQLXML                                                                               =
+      underlying.createSQLXML()
+    def createStatement(resultSetType: Int, resultSetConcurrency: Int, resultSetHoldability: Int): java.sql.Statement =
+      underlying.createStatement(resultSetType, resultSetConcurrency, resultSetHoldability)
+    def createStatement(resultSetType: Int, resultSetConcurrency: Int): java.sql.Statement                            =
+      underlying.createStatement(resultSetType, resultSetConcurrency)
+    def createStatement(): java.sql.Statement                                                                         =
+      underlying.createStatement()
+    def createStruct(typeName: String, attributes: Array[Object]): java.sql.Struct                                    =
+      underlying.createStruct(typeName, attributes)
+    def getAutoCommit(): Boolean                                                                                      =
+      underlying.getAutoCommit()
+    def getCatalog(): String                                                                                          =
+      underlying.getCatalog()
+    def getClientInfo(): java.util.Properties                                                                         =
+      underlying.getClientInfo()
+    def getClientInfo(name: String): String                                                                           =
+      underlying.getClientInfo(name)
+    def getHoldability(): Int                                                                                         =
+      underlying.getHoldability()
+    def getMetaData(): java.sql.DatabaseMetaData                                                                      =
+      underlying.getMetaData()
+    def getNetworkTimeout(): Int                                                                                      =
+      underlying.getNetworkTimeout()
+    def getSchema(): String                                                                                           =
+      underlying.getSchema()
+    def getTransactionIsolation(): Int                                                                                =
+      underlying.getTransactionIsolation()
+    def getTypeMap(): java.util.Map[String, Class[_]]                                                                 =
+      underlying.getTypeMap()
+    def getWarnings(): java.sql.SQLWarning                                                                            =
+      underlying.getWarnings()
+    def isClosed(): Boolean                                                                                           =
+      underlying.isClosed()
+    def isReadOnly(): Boolean                                                                                         =
+      underlying.isReadOnly()
+    def isValid(timeout: Int): Boolean                                                                                =
+      underlying.isValid(timeout)
+    def isWrapperFor(iface: Class[_]): Boolean                                                                        =
+      underlying.isWrapperFor(iface)
+    def nativeSQL(sql: String): String                                                                                =
+      underlying.nativeSQL(sql)
+    def prepareCall(
+      sql: String,
+      resultSetType: Int,
+      resultSetConcurrency: Int,
+      resultSetHoldability: Int
+    ): java.sql.CallableStatement =
+      underlying.prepareCall(sql, resultSetType, resultSetConcurrency, resultSetHoldability)
+    def prepareCall(sql: String, resultSetType: Int, resultSetConcurrency: Int): java.sql.CallableStatement           =
+      underlying.prepareCall(sql, resultSetType, resultSetConcurrency)
+    def prepareCall(sql: String): java.sql.CallableStatement                                                          =
+      underlying.prepareCall(sql)
+    def prepareStatement(sql: String, columnNames: Array[String]): java.sql.PreparedStatement                         =
+      underlying.prepareStatement(sql, columnNames)
+    def prepareStatement(sql: String, columnIndexes: Array[Int]): java.sql.PreparedStatement                          =
+      underlying.prepareStatement(sql, columnIndexes)
+    def prepareStatement(sql: String, autoGeneratedKeys: Int): java.sql.PreparedStatement                             =
+      underlying.prepareStatement(sql, autoGeneratedKeys)
+    def prepareStatement(
+      sql: String,
+      resultSetType: Int,
+      resultSetConcurrency: Int,
+      resultSetHoldability: Int
+    ): java.sql.PreparedStatement =
+      underlying.prepareStatement(sql, resultSetType, resultSetConcurrency, resultSetHoldability)
+    def prepareStatement(sql: String, resultSetType: Int, resultSetConcurrency: Int): java.sql.PreparedStatement      =
+      underlying.prepareStatement(sql, resultSetType, resultSetConcurrency)
+    def prepareStatement(sql: String): java.sql.PreparedStatement                                                     =
+      underlying.prepareStatement(sql)
+    def releaseSavepoint(savepoint: java.sql.Savepoint): Unit                                                         =
+      underlying.releaseSavepoint(savepoint)
+    def restore(): Unit                                                                                               = {
       underlying.setAutoCommit(initialAutoCommit)
       underlying.setCatalog(initialCatalog)
       underlying.setClientInfo(initialClientInfo)
@@ -121,114 +209,35 @@ object ZConnection {
       underlying.setSchema(initialSchema)
       underlying.setTransactionIsolation(initialTransactionIsolation)
     }
-
-    def abort(executor: java.util.concurrent.Executor): Unit                                    =
-      underlying.abort(executor)
-    def clearWarnings(): Unit                                                                   =
-      underlying.clearWarnings()
-    def close(): Unit                                                                           =
-      underlying.close()
-    def commit(): Unit                                                                          =
-      underlying.commit()
-    def createArrayOf(x$1: String, x$2: Array[Object]): java.sql.Array                          =
-      underlying.createArrayOf(x$1, x$2)
-    def createBlob(): java.sql.Blob                                                             =
-      underlying.createBlob()
-    def createClob(): java.sql.Clob                                                             =
-      underlying.createClob()
-    def createNClob(): java.sql.NClob                                                           =
-      underlying.createNClob()
-    def createSQLXML(): java.sql.SQLXML                                                         =
-      underlying.createSQLXML()
-    def createStatement(x$1: Int, x$2: Int, x$3: Int): java.sql.Statement                       =
-      underlying.createStatement(x$1, x$2, x$3)
-    def createStatement(x$1: Int, x$2: Int): java.sql.Statement                                 =
-      underlying.createStatement(x$1, x$2)
-    def createStatement(): java.sql.Statement                                                   =
-      underlying.createStatement()
-    def createStruct(x$1: String, x$2: Array[Object]): java.sql.Struct                          =
-      underlying.createStruct(x$1, x$2)
-    def getAutoCommit(): Boolean                                                                =
-      underlying.getAutoCommit()
-    def getCatalog(): String                                                                    =
-      underlying.getCatalog()
-    def getClientInfo(): java.util.Properties                                                   =
-      underlying.getClientInfo()
-    def getClientInfo(x$1: String): String                                                      =
-      underlying.getClientInfo(x$1)
-    def getHoldability(): Int                                                                   =
-      underlying.getHoldability()
-    def getMetaData(): java.sql.DatabaseMetaData                                                =
-      underlying.getMetaData()
-    def getNetworkTimeout(): Int                                                                =
-      underlying.getNetworkTimeout()
-    def getSchema(): String                                                                     =
-      underlying.getSchema()
-    def getTransactionIsolation(): Int                                                          =
-      underlying.getTransactionIsolation()
-    def getTypeMap(): java.util.Map[String, Class[_]]                                           =
-      underlying.getTypeMap()
-    def getWarnings(): java.sql.SQLWarning                                                      =
-      underlying.getWarnings()
-    def isClosed(): Boolean                                                                     =
-      underlying.isClosed()
-    def isReadOnly(): Boolean                                                                   =
-      underlying.isReadOnly()
-    def isValid(x$1: Int): Boolean                                                              =
-      underlying.isValid(x$1)
-    def nativeSQL(x$1: String): String                                                          =
-      underlying.nativeSQL(x$1)
-    def prepareCall(x$1: String, x$2: Int, x$3: Int, x$4: Int): java.sql.CallableStatement      =
-      underlying.prepareCall(x$1, x$2, x$3, x$4)
-    def prepareCall(x$1: String, x$2: Int, x$3: Int): java.sql.CallableStatement                =
-      underlying.prepareCall(x$1, x$2, x$3)
-    def prepareCall(x$1: String): java.sql.CallableStatement                                    =
-      underlying.prepareCall(x$1)
-    def prepareStatement(x$1: String, x$2: Array[String]): java.sql.PreparedStatement           =
-      underlying.prepareStatement(x$1, x$2)
-    def prepareStatement(x$1: String, x$2: Array[Int]): java.sql.PreparedStatement              =
-      underlying.prepareStatement(x$1, x$2)
-    def prepareStatement(x$1: String, x$2: Int): java.sql.PreparedStatement                     =
-      underlying.prepareStatement(x$1, x$2)
-    def prepareStatement(x$1: String, x$2: Int, x$3: Int, x$4: Int): java.sql.PreparedStatement =
-      underlying.prepareStatement(x$1, x$2, x$3, x$4)
-    def prepareStatement(x$1: String, x$2: Int, x$3: Int): java.sql.PreparedStatement           =
-      underlying.prepareStatement(x$1, x$2, x$3)
-    def prepareStatement(x$1: String): java.sql.PreparedStatement                               =
-      underlying.prepareStatement(x$1)
-    def releaseSavepoint(x$1: java.sql.Savepoint): Unit                                         =
-      underlying.releaseSavepoint(x$1)
-    def rollback(x$1: java.sql.Savepoint): Unit                                                 =
-      underlying.rollback(x$1)
-    def rollback(): Unit                                                                        =
+    def rollback(savepoint: java.sql.Savepoint): Unit                                                                 =
+      underlying.rollback(savepoint)
+    def rollback(): Unit                                                                                              =
       underlying.rollback()
-    def setAutoCommit(x$1: Boolean): Unit                                                       =
-      underlying.setAutoCommit(x$1)
-    def setCatalog(x$1: String): Unit                                                           =
-      underlying.setCatalog(x$1)
-    def setClientInfo(x$1: java.util.Properties): Unit                                          =
-      underlying.setClientInfo(x$1)
-    def setClientInfo(x$1: String, x$2: String): Unit                                           =
-      underlying.setClientInfo(x$1, x$2)
-    def setHoldability(x$1: Int): Unit                                                          =
-      underlying.setHoldability(x$1)
-    def setNetworkTimeout(x$1: java.util.concurrent.Executor, x$2: Int): Unit                   =
-      underlying.setNetworkTimeout(x$1, x$2)
-    def setReadOnly(x$1: Boolean): Unit                                                         =
-      underlying.setReadOnly(x$1)
-    def setSavepoint(x$1: String): java.sql.Savepoint                                           =
-      underlying.setSavepoint(x$1)
-    def setSavepoint(): java.sql.Savepoint                                                      =
+    def setAutoCommit(autoCommit: Boolean): Unit                                                                      =
+      underlying.setAutoCommit(autoCommit)
+    def setCatalog(catalog: String): Unit                                                                             =
+      underlying.setCatalog(catalog)
+    def setClientInfo(properties: java.util.Properties): Unit                                                         =
+      underlying.setClientInfo(properties)
+    def setClientInfo(name: String, value: String): Unit                                                              =
+      underlying.setClientInfo(name, value)
+    def setHoldability(holdability: Int): Unit                                                                        =
+      underlying.setHoldability(holdability)
+    def setNetworkTimeout(executor: java.util.concurrent.Executor, milliseconds: Int): Unit                           =
+      underlying.setNetworkTimeout(executor, milliseconds)
+    def setReadOnly(readOnly: Boolean): Unit                                                                          =
+      underlying.setReadOnly(readOnly)
+    def setSavepoint(name: String): java.sql.Savepoint                                                                =
+      underlying.setSavepoint(name)
+    def setSavepoint(): java.sql.Savepoint                                                                            =
       underlying.setSavepoint()
-    def setSchema(x$1: String): Unit                                                            =
-      underlying.setSchema(x$1)
-    def setTransactionIsolation(x$1: Int): Unit                                                 =
-      underlying.setTransactionIsolation(x$1)
-    def setTypeMap(x$1: java.util.Map[String, Class[_]]): Unit                                  =
-      underlying.setTypeMap(x$1)
-    def isWrapperFor(x$1: Class[_]): Boolean                                                    =
-      underlying.isWrapperFor(x$1)
-    def unwrap[T](x$1: Class[T]): T                                                             =
-      underlying.unwrap(x$1)
+    def setSchema(schema: String): Unit                                                                               =
+      underlying.setSchema(schema)
+    def setTransactionIsolation(level: Int): Unit                                                                     =
+      underlying.setTransactionIsolation(level)
+    def setTypeMap(map: java.util.Map[String, Class[_]]): Unit                                                        =
+      underlying.setTypeMap(map)
+    def unwrap[T](iface: Class[T]): T                                                                                 =
+      underlying.unwrap(iface)
   }
 }
